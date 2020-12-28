eec05ce408bb14667761ded452ddf80b71a79ccd
Paweł Szczodrowski <pawelszczo@gmail.com>
2020-12-28 14:50:10 +0100

    first commit

---
+++ Models/BlogDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Blog_project.Models;
+
+namespace Blog_project.Models
+{
+    public class BlogDBContext : DbContext
+    {
+        public BlogDBContext(DbContextOptions<BlogDBContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Post> Post { get; set; }
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog_project.Models
+{
+    public class Post
+    {
+        public int id { get; set; }
+        public string title { get; set;}
+        public string text { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime publishedDate { get; set; }
+    }
+}
