03e2dac22d0ef6b3ec4aad40444e716c8d65ba4d
Piotr Kiedrowski <58557758+KiedrowskiPio@users.noreply.github.com>
2021-01-12 14:36:26 +0100

    Exchanged UserId to UserName

--- Models/Post.cs
+++ Models/Post.cs
@@ -10,8 +10,20 @@ namespace Blog_project.Models
     public class Post
     {
         public int id { get; set; }
-        public int userId { get; set; }
+
+        [DisplayName("User name:")]
+        [RegularExpression(@"^[a-zA-Z0-9''-'\s]*$", ErrorMessage = "Username cannot include special characters")]//+ co najmniej jedna z duzej ^od tego sie zaczyna $koniec
+        [Required]
+        public string userName { get; set; }
+
+        //public int userId { get; set; }
+        [DisplayName("Title:")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Title of the post must have at least 3 characters")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9''-'\s]*$", ErrorMessage = "Title of the post cannot include special characters and must begin with capital letter")]//+ co najmniej jedna z duzej ^od tego sie zaczyna $koniec
+        [Required]
         public string title { get; set;}
+
+        [DisplayName("Post content:")]
         public string text { get; set; }
 
         [DataType(DataType.DateTime)]
